56a03bc34b7ecabc85628d2de6516ce2e17f6e33
ved7997 <ved7997@gmail.com>
2022-04-19 20:05:12 +0300

    Initial commit

---
+++ src/main/java/com/example/cart_19_04_22/Cart190422Application.java
@@ -0,0 +1,13 @@
+package com.example.cart_19_04_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cart190422Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cart190422Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cart_19_04_22/Cart190422ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cart_19_04_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cart190422ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
